4453f35cb4afd353a8995a80a404b97d9504e30f
NCols <nicolas@ncollig.net>
2017-10-26 09:48:37 +0200

    Move get_exchange_rate in index() function

--- ethprice/ethprice/views.py
+++ ethprice/ethprice/views.py
@@ -3,9 +3,8 @@ from coinbase.wallet.client import Client
 
 client = Client('yhKYHa8AkQaHXDMc','quspNs86vAvsRtyuGmE3nC5o9Nki2pqP')
 
-rates = client.get_exchange_rates(currency='ETH')
-eth_rate = rates['rates']['EUR']
-
 def index(request):
+    rates = client.get_exchange_rates(currency='ETH')
+    eth_rate = rates['rates']['EUR']
     return HttpResponse("1 ETH = %s EUR" % eth_rate)
 
